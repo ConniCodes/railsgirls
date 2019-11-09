62620e4ca4c6d0be9916aa977f98dbc7598ce6ed
connicodes <conni.watson@MLH-0161.local>
2019-11-09 16:14:43 +0000

    Reformatted, added comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+    def create
+        @idea = Idea.find(params[:idea_id])
+        @comment = @idea.comments.create(comment_params)
+        redirect_to idea_path(@idea)
+    end
+
+    private
+    def comment_params
+        params.require(:comment) .permit(:name, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/info'
   root to: "ideas#index"
-  resources :ideas
+  resources :ideas do 
+    resources :comments 
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
